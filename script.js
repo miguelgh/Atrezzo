be2f40f2021b44cdd2a026137ca5587de273e13f
miguelgh16 <miguelgh16@gmail.com>
2024-10-20 23:21:22 -0300

    se modifica el boton subir

--- script.js
+++ script.js
@@ -17,3 +17,18 @@ function borrar(){
     cantidad.value = 0;
     costo.innerText = 0;
 }
+
+//boton .subir
+window.onscroll = function(){
+    //console.log(document.documentElement.scrollTop); //con esto se verifica en la consola cuando se hace scroll
+    const botonSubir = document.querySelector('.subir');
+    if(document.documentElement.scrollTop > 100){
+        botonSubir.classList.remove('subir-hide');
+        botonSubir.classList.add('subir-show');
+     //console.log("entro");
+    }else{
+        botonSubir.classList.remove('subir-show');
+        botonSubir.classList.add('subir-hide');
+     //console.log("entro Else");
+    }
+}
