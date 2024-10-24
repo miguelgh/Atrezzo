782892ea0bdd0ed9a4fa72ac55e9cbd866f2dfe1
miguelgh16 <miguelgh16@gmail.com>
2024-10-24 20:18:29 -0300

    desde notebook

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-const precio = 200;
-const cantidad = document.getElementById('cantidad');
-const categoria = document.getElementById('categoria');
-const costo = document.getElementById('costo');
-
-function resumen(){
-    if (cantidad.value >= 0){
-        let valor = cantidad.value * precio;
-        let final = valor - (valor*categoria.value);
-        costo.innerText = final;
-    }else{
-        alert("la cantidad es menor a 0");
-    }
-}
-
-function borrar(){
-    cantidad.value = 0;
-    costo.innerText = 0;
-}
-
 //boton .subir
 window.onscroll = function(){
     //console.log(document.documentElement.scrollTop); //con esto se verifica en la consola cuando se hace scroll
